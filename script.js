98430c065af6929b44721b4ee1c0e848bf5e6998
noyeosa-obazee <obazeenoyeosa@gmail.com>
2025-11-08 18:44:56 +0100

    Add restraint of 100 to user prompt
    Add restraint feature of 100 to user prompt

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
 const container = document.querySelector('#container');
 const chooseBtn = document.querySelector('#choose-btn');
-const arr = []
+const deletedSquareFlexerIds = [];
 
+function getDimension() {
+    const dimension = prompt('Enter number of squares per side for sketch pad (must not be greater than 100):');
+    return dimension;
+}
 
 chooseBtn.addEventListener('click', function() {
-    
-    const dimension = prompt('Enter preferred square dimension:')
-    generateSketchPad(parseInt(dimension));
+    let userDimension = getDimension();
+    if (userDimension > 100) while (userDimension > 100) {
+        userDimension = getDimension();
+    }
+    generateSketchPad(parseInt(userDimension));
 })
 
 function generateSketchPad(n) {
+    const allSquareFlexers = document.querySelectorAll('.square-flexer')
+    allSquareFlexers.forEach(function (e) { if (deletedSquareFlexerIds.includes(e.id)) document.querySelector(`#${e.id}`).remove()});
     
-    const sArr = document.querySelectorAll('.square-flexer')
-    sArr.forEach(function (e) { if (arr.includes(e.id)) document.querySelector(`#${e.id}`).remove()})
-    
-
-        
     let deleteId = crypto.randomUUID();
     const squareFlexer = document.createElement('div');
     squareFlexer.classList.add('square-flexer');
-    deleteId = `gene${deleteId}rated`
-    arr.push(deleteId)
+    deleteId = `gene${deleteId}rated`;
+    deletedSquareFlexerIds.push(deleteId);
     squareFlexer.id = deleteId;
-for (let i = 1; i <= n; i++) {
-    
-    
 
+ for (let i = 1; i <= n; i++) {
     for (let i = 1; i <= n; i++) {
     const square = document.createElement('div');
-    square.classList.add('square')
-    square.style.width = `${1600/n}px`
-    square.style.height = `${1600/n}px`
-    square.addEventListener('mouseover', (e) => e.target.classList.add('hovered'))
+    square.classList.add('square');
+    square.style.width = `${1600/n}px`;
+    square.style.height = `${1600/n}px`;
+    square.addEventListener('mouseover', (e) => e.target.classList.add('hovered'));
     squareFlexer.appendChild(square);
     }
     container.appendChild(squareFlexer);
-    }
-        }
+  }
+}
 
 
 
